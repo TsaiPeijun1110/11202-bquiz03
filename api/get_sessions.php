b1917c9d88b138863cdb84394427a1055f9b0b86
tsai <lovez81427@gmail.com>
2024-01-15 15:16:05 +0800

    增加判斷非當日的場次選擇

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -1,9 +1,9 @@
-<?php include_once "db.php";
+<?php include_once 'db.php';
 $movie=$_GET['movie'];
 $date=$_GET['date'];
 $H=date("G");
-$start=($H<14)?1:7-ceil((24-$H)/2);
+$start=($H>=14 && $date==date("Y-m-d"))?7-ceil((24-$H)/2):1;
 
-for($i=1;$i<=5;$i++){
+for($i=$start;$i<=5;$i++){
     echo "<option value='{$sess[$i]}'>{$sess[$i]} 剩餘座位 20</option>";
 }
