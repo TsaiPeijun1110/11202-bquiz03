e33242ff7552f2c4590de6296e65694479155a41
tsai <lovez81427@gmail.com>
2024-01-15 16:24:01 +0800

    增加判斷網址參數來選中電影的功能

--- front/order.php
+++ front/order.php
@@ -1,18 +1,16 @@
-<style>
-
-</style>
 <h3 class="ct">線上訂票</h3>
 <div class="order">
     <div>
-        <label>電影:</label>
-        <select name="movie" id="movie"></select>
+        <label>電影：</label>
+        <select name="movie" id="movie">
+        </select>
     </div>
     <div>
-        <label>日期:</label>
+        <label>日期：</label>
         <select name="date" id="date"></select>
     </div>
     <div>
-        <label>場次:</label>
+        <label>場次</label>
         <select name="session" id="session"></select>
     </div>
     <div>
@@ -21,33 +19,39 @@
     </div>
 </div>
 <script>
-    getMovies();
+let url=new URL(window.location.href)
 
-    $("#movie").on("change",function(){
+getMovies();
+
+$("#movie").on("change",function(){
+    getDates($("#movie").val())
+})
+
+$("#date").on("change",function(){
+    getSessions($("#movie").val(),$("#date").val())
+})
+
+function getMovies(){
+    $.get("./api/get_movies.php",(movies)=>{
+        $("#movie").html(movies);
+        if(url.searchParams.has('id')){
+           $(`#movie option[value='${url.searchParams.get('id')}']`).prop('selected',true);
+        }
         getDates($("#movie").val())
     })
-
-    $("#date").on("change",function(){
-        getSessions($("#movie").val(),$("#date").val())
-    })
-
-    function getMovies(){
-        $.get("./api/get_movies.php",(movies)=>{
-            $("#movie").html(movies);
-            getDates($("#movie").val())
-        })
-    }
-    function getDates(id){
-        $.get("./api/get_dates.php",{id},(dates)=>{
+}
+function getDates(id){
+    $.get("./api/get_dates.php",{id},(dates)=>{
             $("#date").html(dates);
-            let movie=$("#movie").val();
-            let date=$("#date").val();
+            let movie=$("#movie").val()
+            let date=$("#date").val()
             getSessions(movie,date)
-        })
-    }
-    function getSessions(movie,date){
-        $.get("./api/get_sessions.php",{movie,date},(sessions)=>{
+    })
+}
+function getSessions(movie,date){
+    $.get("./api/get_sessions.php",{movie,date},(sessions)=>{
             $("#session").html(sessions);
-        })
-    }
+    })
+}
+
 </script>
